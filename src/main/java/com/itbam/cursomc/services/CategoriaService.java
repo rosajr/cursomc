4e8e3a2058852febe474ba204f641b9c941ab23e
Joao Rosa <jrrosa@live.com>
2019-06-03 08:10:25 -0400

    Ajustes no endpoint /categorias/id

--- src/main/java/com/itbam/cursomc/services/CategoriaService.java
+++ src/main/java/com/itbam/cursomc/services/CategoriaService.java
@@ -2,12 +2,16 @@ package com.itbam.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itbam.cursomc.domain.Categoria;
 import com.itbam.cursomc.repositories.CategoriaRepository;
+import com.itbam.cursomc.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class CategoriaService {
@@ -18,7 +22,9 @@ public class CategoriaService {
 	@RequestMapping("/{id}")
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 		
 	}
