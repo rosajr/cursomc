175323dca2b3253ea68d6b47adf39a90be46d477
Joao Rosa <jrrosa@live.com>
2019-06-04 08:52:54 -0400

    Cliente, tipo cliente, telefones e enderecos

---
+++ src/main/java/com/itbam/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,12 @@
+package com.itbam.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.itbam.cursomc.domain.Cliente;
+import com.itbam.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+}
